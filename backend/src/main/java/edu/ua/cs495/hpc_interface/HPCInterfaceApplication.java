9648197a49f93abe978780df1b9fa4e56bfa144d
Noah Overcash <ncovercash@ua.edu>
2022-09-13 15:19:47 -0500

    Add launch config

--- backend/src/main/java/edu/ua/cs495/hpc_interface/HPCInterfaceApplication.java
+++ backend/src/main/java/edu/ua/cs495/hpc_interface/HPCInterfaceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HpcInterfaceApplication {
+public class HPCInterfaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HpcInterfaceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HPCInterfaceApplication.class, args);
+  }
 }

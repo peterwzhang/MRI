d8d52d84071523b69ba785085cea490abcf9f6c7
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-09-16 14:52:07 -0500

    Changed pages format

--- interface/pages/create-job.tsx
+++ interface/pages/create-job.tsx
@@ -4,22 +4,19 @@ import HeroBanner from "../components/HeroBanner"
 import JobForm from "../components/JobForm";
 import Navbar from '../components/Navbar';
 import SectionDiv from "../components/SectionDiv";
-import styles from '../styles/Home.module.css'
 
 export default function CreateJob(){
     return(
-        <div className={styles.main}>
+        <div>
             <Head>
-            <title>Create a Job</title>
-            <meta name="description" content="CS 495 Capstone project" />
-            <link rel="icon" href="/favicon.ico" />
+                <title>Create a Job</title>
+                <meta name="description" content="CS 495 Capstone project" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <HeroBanner />
             <Navbar/>
-            <h1 className={styles.title}>Create a job here!</h1>
-            <p>Future Content: form with necessary parameters for starting jobs on HPC cluster</p>
             <SectionDiv>
-            <JobForm />
+                <JobForm />
             </SectionDiv>
         </div>
     )

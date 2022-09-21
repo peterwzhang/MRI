5796ecd5c8dc31dd1d76ee32ea52e7f91868d903
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-09-21 00:25:45 -0500

    Added table, buttons, and progress bar for dashboard

--- interface/components/BatchTable.tsx
+++ interface/components/BatchTable.tsx
@@ -3,39 +3,24 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'status', headerName: 'Status', width: 130 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, name: 'Snow', status: '60%'},
+  { id: 2, name: 'Lannister', status: '0%'},
+  { id: 3, name: 'Lannister', status: '0%'},
+  { id: 4, name: 'Stark', status: 'done'},
+  { id: 5, name: 'Targaryen', status: '0%'},
+  { id: 6, name: 'Melisandre', status: null},
+  { id: 7, name: 'Clifford', status: '0%'},
+  { id: 8, name: 'Frances', status: '0%'},
+  { id: 9, name: 'Roxie', status: '0%'},
 ];
 
 const BatchTable = () => (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 370, width: '50%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
--- interface/pages/dashboard.tsx
+++ interface/pages/dashboard.tsx
@@ -4,6 +4,8 @@ import HeroBanner from "../components/HeroBanner"
 import BatchTable from '../components/BatchTable';
 import Navbar from '../components/Navbar';
 import SectionDiv from "../components/SectionDiv";
+import styled from "styled-components";
+import ProgressBar from "../components/ProgressBar";
 
 export default function Dashboard(){
     return(
@@ -17,9 +19,35 @@ export default function Dashboard(){
             <Navbar/>
             <SectionDiv>
                 <h1>Batches</h1>
-                <BatchTable />   
+                <ProgressBar/>
+                <Wrapper>
+                    <BatchTable />   
+                    <Buttons>
+                        <Button>Cancel</Button>
+                        <Button>Refresh</Button>
+                        <Button>View</Button>
+                    </Buttons>
+                </Wrapper>
             </SectionDiv>
         </div>
     )
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    gap: 4rem;
+`
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 20rem;
+`
+const Button = styled.button`
+    border: none;
+    height: 4rem;
+    font-size: 1em;
+`
 

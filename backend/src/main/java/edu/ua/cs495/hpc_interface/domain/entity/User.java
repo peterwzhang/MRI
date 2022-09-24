57415ebced56cb6a702c3b620b2331dc7508cc56
Noah Overcash <ncovercash@ua.edu>
2022-09-23 23:20:55 -0500

    Add admin field to user info

--- backend/src/main/java/edu/ua/cs495/hpc_interface/domain/entity/User.java
+++ backend/src/main/java/edu/ua/cs495/hpc_interface/domain/entity/User.java
@@ -25,26 +25,38 @@ import org.hibernate.annotations.Type;
 @Table(name = "users")
 public class User implements Serializable {
 
+  /** Unique ID for this user */
   @Id
   @NotNull
   @GeneratedValue
   @Column(name = "id")
   private UUID id;
 
+  /** The myBama ID for this user */
   @NotNull
   @Column(name = "username")
   private String username;
 
+  /** The user's email address (for notifications and other such features) */
   @NotNull
   @Column(name = "email")
   private String email;
 
+  /** If the user is an administrator */
+  @Builder.Default
+  @Column(name = "admin")
+  private boolean admin = false;
+
+  /**
+   * The private key that should be used to access the HPC cluster on the user's behalf
+   */
   @Lob
   @NotNull
   @Column(name = "private_key")
   @Type(type = "org.hibernate.type.BinaryType")
   private byte[] privateKey;
 
+  /** The public key that should be installed on the HPC cluster by the user */
   @Lob
   @NotNull
   @Column(name = "public_key")

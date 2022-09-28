14ca816b2bbf95b556c0e0ec441e556b1bd8da57
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-09-28 18:52:10 -0500

    Added react query and react query devtools

--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    );
 }
 
 export default App;

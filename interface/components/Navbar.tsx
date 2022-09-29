8d18b189fcac3fd1e99bff15a236f958fd607729
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-09-29 14:27:36 -0500

    Added script library tab

--- interface/components/Navbar.tsx
+++ interface/components/Navbar.tsx
@@ -24,6 +24,11 @@ const Navbar = () => (
                 </div>
                 <div className="nav_button">
                     <h3 className="nav_link">
+                        <Link href="/script-library">Script library</Link>
+                    </h3>
+                </div>
+                <div className="nav_button">
+                    <h3 className="nav_link">
                         <Link href="/view-batch">View batch</Link>
                     </h3>
                 </div>

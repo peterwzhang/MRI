b2bb9e16a3165649580ce572bfcdf7aa50011493
cacannarozzi <cacannarozzi@crimson.ua.edu>
2022-10-10 19:43:02 -0500

    Update View Batch page layout

--- interface/pages/view-batch.tsx
+++ interface/pages/view-batch.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Navbar from '../components/Navbar';
 import SectionDiv from "../components/SectionDiv";
+import BatchSelect from "../components/BatchSelect";
 
 export default function ViewBatch(){
     return(
         <div>
             <Navbar/>
             <SectionDiv>
-                <h1>View Batch</h1>
-                <label>Select batch to view: </label>
-                <select name="batchName">
-                    <option value="slurm-test.sh">Slurm Test</option>
-                </select>
+                <BatchSelect/>
             </SectionDiv>
         </div>
     )

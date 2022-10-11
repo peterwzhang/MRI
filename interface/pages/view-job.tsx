00fc311fc8c33981af282133c2dd61eeac9931b6
cacannarozzi <cacannarozzi@crimson.ua.edu>
2022-10-10 19:55:21 -0500

    Move job information to component

--- interface/pages/view-job.tsx
+++ interface/pages/view-job.tsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import SectionDiv from "../components/SectionDiv";
 import ProgressBar from "../components/ProgressBar";
 import JobSelect from "../components/JobSelect";
+import JobInfo from "../components/JobInfo";
 
 export default function ViewJob(){
 
@@ -14,18 +15,7 @@ export default function ViewJob(){
                 <JobSelect/>
             </SectionDiv>
             <SectionDiv>
-                <h3>Member Batch</h3>
-                <p>Snow</p>
-                <h3>Job Status</h3>
-                <p>In Progress</p>
-                <h3>Job Progress</h3>
-                <ProgressBar/>
-                <h3>Current Runtime</h3>
-                <p>100 hours</p>
-                <h3>Job Log (last 50KB)</h3>
-                <Prism>Example: analyzing image 29</Prism>
-                <Prism>Example: running Job 1</Prism>
-                <Prism>Example: Job 0 completed. Starting new job now</Prism>
+                <JobInfo/>
             </SectionDiv>
         </div>
     )

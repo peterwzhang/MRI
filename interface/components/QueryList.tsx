ae7bcef9d4c268b243db23e73e884e222aa0374d
cacannarozzi <cacannarozzi@crimson.ua.edu>
2022-10-17 23:58:30 -0500

    Reformat script library page, add list of scripts and dropdown option

--- interface/components/QueryList.tsx
+++ interface/components/QueryList.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 import { BatchMetadata } from '../types';
-const QuerySelect = ({restUrl}: {restUrl: string}) => {
+const QueryList = ({restUrl}: {restUrl: string}) => {
     const { isLoading, error, data } = useQuery(['data'], () =>
     fetch(restUrl).then(res =>
       res.json()
@@ -10,10 +10,9 @@ const QuerySelect = ({restUrl}: {restUrl: string}) => {
   if (isLoading) return <h1>Loading...</h1>
 
   if (error) return <h1>Error</h1>
-//   return data[0].name
-  return (<select name="batchType">
-    {data.map((d: BatchMetadata) => <option key={d.name} value={d.name}>{d.name}</option>)}
-</select>)
+  return (<ul>
+    {data.map((d: BatchMetadata) => <li key={d.name} value={d.name}>{d.name}</li>)}
+  </ul>)
 }
 
-export default QuerySelect;
+export default QueryList;
--- interface/pages/script-library.tsx
+++ interface/pages/script-library.tsx
@@ -2,36 +2,42 @@ import React from "react";
 import SectionDiv from "../components/SectionDiv";
 import styled from "styled-components";
 import QueryList from "../components/QueryList"
+import QuerySelect from "../components/QuerySelect";
 
 export default function ScriptLibrary(){
     return(
-        <SectionDiv>
-            <h1>Script Library</h1>
-            <Buttons>
+        <div>
+            <SectionDiv>
+                <h1>Script Library</h1>
+                <h3>List of Scripts</h3>
+                <QueryList restUrl='https://localhost:8443/api/scripts'></QueryList>
+            </SectionDiv>
+            <SectionDiv>
+                <h3>Upload a Script</h3>
                 <Button>
                     Upload script
                 </Button>
-                <label>Select Script to Export: </label>
-                <QueryList restUrl='https://localhost:8443/api/scripts'></QueryList>
+            </SectionDiv>
+            <SectionDiv>
+                <h3>Export a Script</h3>
+                <label>Select a script to export: </label>
+                <QuerySelect restUrl='https://localhost:8443/api/scripts'></QuerySelect>
+                <p></p>
                 <Button>
                     Export script
                 </Button>
-            </Buttons>
-        </SectionDiv>
+            </SectionDiv>
+        </div>
     )
 }
 
-const Buttons = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    width: 20rem;
-`
 const Button = styled.button`
+    flext-wrap: wrap;
     border-width: .05rem;
     border-radius: .5rem;
     padding: 1rem;
-    width: 80%;
+    cursor: pointer;
+    width: 16rem;
     font-size: 1rem;
 `
 

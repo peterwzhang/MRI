f12eae2e6b51254665e7eac1cba7da34a2be66e5
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-10-18 09:29:34 -0500

    Remove redirect

--- interface/next.config.js
+++ interface/next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'https://localhost:8443/',
-        permanent: false,
-        basePath: false
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import Head from "next/head";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'

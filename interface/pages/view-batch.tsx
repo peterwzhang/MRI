cdcddc227de3398168d7ad7666b1d542e090464b
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-10-19 21:15:40 -0500

    fix batch type

--- interface/pages/view-batch.tsx
+++ interface/pages/view-batch.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import SectionDiv from "../components/SectionDiv";
 import BatchSelect from "../components/BatchSelect";
 import BatchInfo from "../components/BatchInfo";
-import { BatchMetadata } from "../types";
+import { BatchMetadataWithId } from "../types";
 
 export default function ViewBatch(){
-    const [batch, setBatch] = useState<BatchMetadata | undefined>(undefined)
+    const [batch, setBatch] = useState<BatchMetadataWithId | undefined>(undefined)
 
     return(
         <div>

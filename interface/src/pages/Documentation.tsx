c5d761e08a6daf322d677037d29cafc2609942e2
Madeline Moore <mcmoore11@crimson.ua.edu>
2022-11-08 15:43:21 -0600

    Removed first part of account setup steps

--- interface/src/pages/Documentation.tsx
+++ interface/src/pages/Documentation.tsx
@@ -8,27 +8,8 @@ export default function Documentation() {
       {user && (
         <SectionDiv>
           <h1>Setting up your HPC Account</h1>
-          <p>
-            Before you can use the HPC Interface, you must set up your HPC account using the following steps:
-            <ol>
-              <li>Get access to UA&apos;s HPC Portal</li>
-              <ol>
-                <li>If you haven&apos;t already, email Dr. Rishi Deshpande hddeshpande@ua.edu with your CWID</li>
-                <li>Submit a Research Computing Portal Request to UA&apos;s Office of Information Technology here:
-                  <p>
-                    <a href="https://research-computing-portal.oitapps.ua.edu/noneMember" target="_blank" rel="noreferrer">
-                      UAHPC Request
-                    </a>
-                  </p>
-                </li>
-              </ol>
-              <li>Set up SSH</li>
-              <ol>
-                <li>Copy and paste the following into your computer&apos;s Terminal application
-                  <p style={{ maxWidth: "75ch", overflowWrap: "break-word", background: "white" }}>echo {user.publicKey} &gt; tmp.pub; ssh-copy-id -fi tmp.pub {user.username}@uahpc.ua.edu; rm tmp.pub</p>
-                </li>
-              </ol>
-            </ol>
+          <p>Before you can use the HPC Interface, you must set up your HPC account. To do so, simply copy and paste the following into your computer&apos;s Terminal application:
+            <p style={{ maxWidth: "75ch", overflowWrap: "break-word", background: "white" }}>echo {user.publicKey} &gt; tmp.pub; ssh-copy-id -fi tmp.pub {user.username}@uahpc.ua.edu; rm tmp.pub</p>
           </p>
         </SectionDiv>
       )}
